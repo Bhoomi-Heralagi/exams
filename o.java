98f0f4591f6510f46afdba349ecaf865b341baa2
Bhoomi-Heralagi <bhoomiheral46g@gmail.com>
2024-05-16 11:24:28 +0530

    project

---
+++ o.java
@@ -0,0 +1,10 @@
+public class o {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to gpt");
+		System.out.println("welcome to gpt");
+		System.out.println("welcome to gpt");
+		System.out.println("welcome to gpt");
+	}
+
+}
